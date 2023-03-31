6f5ac4cd6cf99f7673e291ccb0f7f9525d2d827e
Xifted <oneisfirst01@gmail.com>
2023-03-31 16:59:50 +0700

    Small Update 31/03/2023

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -37,9 +37,13 @@ class RoomsController extends Controller
         $cari = $request->cari;
 
         // mengambil data dari table tipe sesuai pencarian data
-        $LTipe = DB::table('tipe_kamar')
-            ->where('nama', 'like', "%" . $cari . "%")
-            ->paginate();
+        
+        $LTipe = DB::table('kamar')->select('id_kamar', 'kamar.nama AS namaKamar', 'tipe_kamar.*')->where('kamar.status', '=', 'tersedia')->join('tipe_kamar', 'kamar.id_tipe', '=', 'tipe_kamar.id_tipe')
+        ->where('nama', 'like', "%" . $cari . "%")
+        ->paginate();
+        // $LTipe = DB::table('tipe_kamar')
+        //     ->where('nama', 'like', "%" . $cari . "%")
+        //     ->paginate();
 
         $KQty = DB::table('kamar')->select(array('id_tipe', DB::raw('COUNT(id_tipe) AS maxQty')))->where('status', '=', 'Tersedia')->groupBy('id_tipe')->get();
 
@@ -116,33 +120,34 @@ class RoomsController extends Controller
         $listDetail = DB::table('detail_reservasi')->select('detail_reservasi.*', 'kamar.nama as namaKamar', 'tipe_kamar.nama as namaTipe', 'tipe_kamar.img_url as imgKamar')->where('id_rsv', '=', $id)->join('kamar', 'detail_reservasi.id_kamar', '=', 'kamar.id_kamar')->join('tipe_kamar', 'kamar.id_tipe', '=', 'tipe_kamar.id_tipe')->get();
 
         // return dd($listDetail);
-        // // Set your Merchant Server Key
-        // \Midtrans\Config::$serverKey = 'YOUR_SERVER_KEY';
-        // // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        // \Midtrans\Config::$isProduction = false;
-        // // Set sanitization on (default)
-        // \Midtrans\Config::$isSanitized = true;
-        // // Set 3DS transaction for credit card to true
-        // \Midtrans\Config::$is3ds = true;
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = 'SB-Mid-server-6LCs4lAxHv6yT1byiO1viTW2';
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
 
-        // $params = array(
-        //     'transaction_details' => array(
-        //         'order_id' => rand(),
-        //         'gross_amount' => 10000,
-        //     ),
-        //     'customer_details' => array(
-        //         'first_name' => 'budi',
-        //         'last_name' => 'pratama',
-        //         'email' => 'budi.pra@example.com',
-        //         'phone' => '08111222333',
-        //     ),
-        // );
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => rand(),
+                'gross_amount' => 10000,
+            ),
+            'customer_details' => array(
+                'first_name' => 'budi',
+                'last_name' => 'pratama',
+                'email' => 'budi.pra@example.com',
+                'phone' => '08111222333',
+            ),
+        );
 
-        // $snapToken = \Midtrans\Snap::getSnapToken($params);
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
 
         return view('tamu.transaksi', [
             'listDetail' => $listDetail,
-            'id_rsv' => $idRsv
+            'id_rsv' => $idRsv,
+            'snap_token' => $snapToken
         ]);
     }
 
